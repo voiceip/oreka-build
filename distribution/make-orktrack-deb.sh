6bf29778a7faba068923e6814e9593d9cc771879
Kinshuk Bairagi <me@kinshuk.in>
2019-08-27 14:13:59 +0530

    orktrack packaging

---
+++ distribution/make-orktrack-deb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+BUILD_ROOT=$(mktemp -d)
+VERSION=$(date +%s)
+cp -r deb/orktrack/* $BUILD_ROOT/
+
+mkdir -p $BUILD_ROOT/var/lib/tomcat8/webapps/
+
+cp orktrack-0.0.1-SNAPSHOT.war $BUILD_ROOT/var/lib/tomcat8/webapps/orktrack.war
+
+
+find $BUILD_ROOT -name '.ignore' -delete
+
+sed -i "s/_VERSION_/$VERSION/g" $BUILD_ROOT/DEBIAN/control
+dpkg-deb --build $BUILD_ROOT orktrack.deb
+
+rm -rf $BUILD_ROOT
